d2ad6c0cfaafbb1a29e912b24afc155bd5a75831
Justin Jameson <jjameson@uw.edu>
2019-02-23 13:16:10 -0800

    got tags to work finally

--- students/JustinJameson/justinjameson_assignment_07/html_render.py
+++ students/JustinJameson/justinjameson_assignment_07/html_render.py
@@ -7,12 +7,34 @@ A class-based system for rendering html.
 
 # This is the framework for the base class
 class Element(object):
-
-    def __init__(self, content=None):
-        pass
+    tag = 'html'
+    def __init__(self, content=''):
+        self.contents = [content]
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write("<{}>\n".format(self.tag))
+        for content in self.contents:
+            out_file.write(content)
+            out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
+
+
+class Html (Element):
+    """Order of tags
+    <!DOCTYPE html> declaration defines this document to be HTML5
+    <html> element is the root element of an HTML page
+    <head> element contains meta information about the document
+    <title> element specifies a title for the document
+    <body> element contains the visible page content
+    <h1> element defines a large heading
+    <p> element defines a paragraph"""
+    tag = 'html'
+
+class Body (Element):
+    tag = 'body'
+
+class P (Element):
+    tag = 'p'
